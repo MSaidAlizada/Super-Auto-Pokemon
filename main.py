17b344561d4f710a54273bf9ca4f145f56a538c2
Said_Alizade <saidjavanshir@gmail.com>
2023-11-23 21:37:59 +0300

    Button Class Created, Function added to draw the button with text and to check for hovering

--- main.py
+++ main.py
@@ -23,9 +23,34 @@ def displayText(text, f, x, y, color):
     textRect.center = (x, y)
     screen.blit(text, textRect)
 
+#Buttons
+class Button:
+    def __init__(self,x,y,width, height, color, textColor, text, textFont):
+        self.x = x-width//2
+        self.y = y-height//2
+        self.width = width
+        self.height = height
+        self.Rectangle = pygame.Rect(self.x,self.y,width,height)
+        self.color = color
+        self.textColor = textColor
+        self.text = text
+        self.textFont= textFont
+    def drawAndClick(self):
+        #draw
+        pygame.draw.rect(screen, self.color, self.Rectangle)
+        pygame.draw.rect(screen, (0,0,0), self.Rectangle,5)
+        displayText(self.text, self.textFont, self.x+self.width//2, self.y+self.height//2, self.textColor)
+        #check for hover
+        if StartButton.Rectangle.collidepoint(pygame.mouse.get_pos()):
+            self.Rectangle = pygame.Rect(self.x-2,self.y-2,self.width+4,self.height+4)
+        else:
+            self.Rectangle = pygame.Rect(self.x,self.y,self.width,self.height)
+
 #states
+StartButton = Button(width//2, height//1.5, 150,100, (250,91,5), (101, 30, 0), "START", textFont)
 def title():
     displayText('Super Auto Pokemon', titleFont, width//2, height//3, (249,188,5) )
+    StartButton.drawAndClick()
 
 #main game loop
 running = True
@@ -41,7 +66,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    
     # Flip the display
     pygame.display.flip()
 
